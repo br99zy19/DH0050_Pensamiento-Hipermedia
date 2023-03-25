1a32852825304756fe5dc796aa575f6bd77e7613
br99zy19 <126963674+br99zy19@users.noreply.github.com>
2023-03-25 10:45:24 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,30 +1,68 @@
+// Variables para los carros
+var car1, car2;
+
 function setup() {
-  let d = 70;
-  let p1 = d;
-  let p2 = p1 + d;
-  let p3 = p2 + d;
-  let p4 = p3 + d;
-
-  // Define el lienzo de 720 pixeles de ancho y 400 de alto
-  createCanvas(720, 400);
-  background(0);
-  noSmooth();
-
-  translate(140, 0);
-
-  // Dibuja una caja gris
-  stroke(153);
-  line(p3, p3, p2, p3);
-  line(p2, p3, p2, p2);
-  line(p2, p2, p3, p2);
-  line(p3, p2, p3, p3);
-
-  // Dibuja puntos blancos
-  stroke(255);
-  point(p1, p1);
-  point(p1, p3);
-  point(p2, p4);
-  point(p3, p1);
-  point(p4, p2);
-  point(p4, p4);
+  // Crear el lienzo
+  createCanvas(600, 400);
+  
+  // Crear los objetos carro con características distintas
+  car1 = new Car(color(255, 0, 0), 1, 0); // Rojo, velocidad 1, no puede saltar
+  car2 = new Car(color(0, 255, 0), 2, 32); // Verde, velocidad 2, puede saltar con la tecla espacio
 }
+
+function draw() {
+  // Establecer el fondo
+  background(220);
+  
+  // Mover los carros
+  car1.move();
+  car2.move();
+  
+  // Mostrar los carros
+  car1.display();
+  car2.display();
+}
+
+function keyPressed() {
+  // Si se presiona la tecla espacio y el carro 2 puede saltar
+  if (keyCode === 32 && car2.canJump) {
+    car2.jump();
+  }
+}
+
+// Clase Car para crear los objetos carro
+class Car {
+  constructor(color, speed, jumpHeight) {
+    this.color = color; // Color del carro
+    this.speed = speed; // Velocidad del carro
+    this.jumpHeight = jumpHeight; // Altura del salto del carro
+    this.canJump = (jumpHeight > 0); // Si el carro puede saltar
+    this.x = 0; // Posición horizontal del carro
+    this.y = 0; // Posición vertical del carro
+  }
+  
+  // Método para mover el carro en el eje x
+  move() {
+    this.x += this.speed;
+    // Si el carro se sale del lienzo, volver al principio
+    if (this.x > width) {
+      this.x = 0;
+    }
+  }
+  
+  // Método para mostrar el carro
+  display() {
+    fill(this.color);
+    rect(this.x, height/2, 50, 30);
+  }
+  
+  // Método para hacer que el carro salte
+  jump() {
+    this.y -= this.jumpHeight;
+    // Si el carro llega al suelo, volver a la posición original
+    if (this.y >= 0) {
+      this.y = 0;
+    }
+  }
+}
+
